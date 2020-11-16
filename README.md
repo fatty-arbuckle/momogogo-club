91699d9d668e9e03c5bd886e3f97fced14ac79d8
Fatty Arbuckle <fatty.arbuckle.87@gmail.com>
2020-11-16 09:13:41 -0500

    Adds bulma and some Barbie

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Momogogo
 
+## Todo
+
+* link/redirect from sign in to posts
+* how to get user info in a Live page?
+* secure db connection
+* test deployment
+* Limit editing to user's own posts
+* Toggle showing all vs own
+* Selection drop down
+* Conversion from activity/time to steps
+* Pretty leaf rating of activity
+* Show user's activity by week/month/etc
+* Show all activity by week/month/etc by user
+
+- Yellow  #ffd545
+- Purple  #fe4eb8
+
+# Notes
+
 ## Timeline
 
 Contains `Post`s of activity
+
+## References
+
+https://www.phoenixframework.org/blog/build-a-real-time-twitter-clone-in-15-minutes-with-live-view-and-phoenix-1-5
+https://github.com/danschultzer/pow
+https://www.youtube.com/watch?v=hnD0Z0LGMIk
--- lib/momogogo_web/live/post_live/post_component.ex
+++ lib/momogogo_web/live/post_live/post_component.ex
@@ -2,17 +2,20 @@ defmodule MomogogoWeb.PostLive.PostComponent do
   use MomogogoWeb, :live_component
 
   def render(assigns) do
+
     ~L"""
-    <div>
-      <row id="post-<%= @post.id %>">
-        <column><%= @post.username %></column>
-        <column><%= @post.date %></column>
-        <column><%= @post.activity %></column>
-        <column><%= @post.duration %></column>
-        <column><%= @post.steps %></column>
-        <column><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, @post) %></column>
-        <column><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></column>
-      </row>
+    <div class="box">
+      <div class="level">
+
+        <span class=level-item"><%= @post.date %></span>
+        <span class=level-item"><%= @post.activity %></span>
+        <span class=level-item"><%= @post.duration %></span>
+        <span class=level-item"><%= @post.steps %></span>
+        <span class=level-item"></span>
+        <span class=level-item"><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, @post) %></span>
+        <span class=level-item"><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></span>
+
+      </div>
     </div>
     """
   end
