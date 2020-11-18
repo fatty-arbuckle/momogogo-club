c93b4f940b5ac1b97d58499d31914f903831498c
Fatty Arbuckle <fatty.arbuckle.87@gmail.com>
2020-11-17 20:47:49 -0500

    Changes to enable early prod testing

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -35,7 +35,7 @@ config :momogogo, MomogogoWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :momogogo, MomogogoWeb.Endpoint, server: true
+config :momogogo, MomogogoWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
