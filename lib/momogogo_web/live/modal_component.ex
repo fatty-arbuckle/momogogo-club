55057314628549c8573fad68d83b1deab717cd7f
Fatty Arbuckle <fatty.arbuckle.87@gmail.com>
2020-11-18 15:59:53 -0500

    Better display of the timeline and post creation

--- lib/momogogo_web/live/modal_component.ex
+++ lib/momogogo_web/live/modal_component.ex
@@ -19,6 +19,23 @@ defmodule MomogogoWeb.ModalComponent do
     """
   end
 
+#   <div class="modal">
+#   <div class="modal-background"></div>
+#   <div class="modal-card">
+#     <header class="modal-card-head">
+#       <p class="modal-card-title">Modal title</p>
+#       <button class="delete" aria-label="close"></button>
+#     </header>
+#     <section class="modal-card-body">
+#       <!-- Content ... -->
+#     </section>
+#     <footer class="modal-card-foot">
+#       <button class="button is-success">Save changes</button>
+#       <button class="button">Cancel</button>
+#     </footer>
+#   </div>
+# </div>
+
   @impl true
   def handle_event("close", _, socket) do
     {:noreply, push_patch(socket, to: socket.assigns.return_to)}
--- lib/momogogo_web/live/post_live/post_component.ex
+++ lib/momogogo_web/live/post_live/post_component.ex
@@ -10,4 +10,11 @@ defmodule MomogogoWeb.PostLive.PostComponent do
    }
   end
 
+  def render_steps(steps) when steps > 15_000, do: "fa-dizzy"
+  def render_steps(steps) when steps > 10_000, do: "fa-grin-stars"
+  def render_steps(steps) when steps >  6_000, do: "fa-smile"
+  def render_steps(steps) when steps >  4_000, do: "fa-meh"
+  def render_steps(steps) when steps >  2_000, do: "fa-frown"
+  def render_steps(steps), do: "fa-meh-rolling-eyes"
+
 end
